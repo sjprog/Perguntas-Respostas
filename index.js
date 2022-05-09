2a3dfe84f6f090df19e530282bd23ca95b216537
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-05-09 17:57:32 -0300

    ajustando

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 
+// Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
 
 
 app.get("/",(req, res) =>{
-    res.send("Bem vindo a Bat caverna!");
+    res.render("index");
 
 });
 
 app.listen(8080,()=>{
-    console.log("App rodando!");
+    console.log("App Rodando!");
 })
