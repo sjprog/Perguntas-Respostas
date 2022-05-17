1ad6832c67b5fd69586a87ebe7199d3f6c59017d
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-05-17 18:32:20 -0300

    complementando

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
-    var nome = ""Sidney";
+    var nome = "Sidney";
     var lang = "Javascript";
-    res.render("index, { }");
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "siqueira",
+        inscritos: 84150
+     });
 
 });
-
+ 
 app.listen(8080, () => {
     console.log("App Rodando!");
 });
