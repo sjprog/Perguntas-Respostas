d54922d5777dab2584f3a7dddafa62e1f741fcb7
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-05-17 18:37:01 -0300

    estudando

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
-app.get("/", (req, res) => {
-    var nome = "Sidney";
-    var lang = "Javascript";
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
     res.render("index", {
         nome: nome,
         lang: lang,
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
         inscritos: 84150
      });
 
-});
+}); 
  
 app.listen(8080, () => {
     console.log("App Rodando!");
