5e24d3d54bb9bd4354645f93a403b189b62113dd
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-05-17 18:52:55 -0300

    estudando

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ app.set('view engine', 'ejs');
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        { nome: "Games", preco: 7.435 },
+        { nome: "Cerveja", preco: 4.50 },
+        {nome: "Whey", preco: 350.00}
+]
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "siqueira",
-        inscritos: 84150
+        inscritos: 84150,
+        msg: exibirMsg,
+        produtos: produtos
+
      });
 
 }); 
