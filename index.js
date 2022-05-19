847aaf951cba2a5a53d73a55efd5874f648c4a32
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-05-19 17:24:04 -0300

    post

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Rotas
 
 app.get("/", (req, res) => { 
 
@@ -14,8 +18,14 @@ app.get("/", (req, res) => {
  
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
-})
+});
 
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário Recebido! titulo " + titulo + " " + " descricao " + descricao);
+    
+});
 
 app.listen(8080, () => {
     console.log("App Rodando!");
