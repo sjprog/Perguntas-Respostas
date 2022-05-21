6a11ad23b22243434162cbd97057cd81b14d3f95
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-05-21 12:53:48 -0300

    redirect

--- index.js
+++ index.js
@@ -58,6 +58,19 @@ app.post("/salvarpergunta", (req, res) => {
     
 });
 
+app.get("/pergunta/:id", (req, res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: { id: id }
+    }).then(pergunta => {
+        if (pergunta != undefined) { // Pergunta foi encontrada.
+            res.render("pergunta");
+        } else { //Não foi encontrada.
+            res.redirect("/");
+        }
+    });
+});
+
 app.listen(8080, () => {
     console.log("App Rodando!");
 });
